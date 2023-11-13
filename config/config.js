ed18ce2fcbad10113bc902f9f7eb2be526fecc49
24aeronyx <11211017@student.itk.ac.id>
2023-11-14 01:36:27 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -14,10 +14,10 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
+    username: "freedb_Aeronyx",
+    password: "dysBq*HacYFP9*2",
+    database: "freedb_todoApp",
+    host: "sql.freedb.tech",
     dialect: "mysql",
   },
 };
