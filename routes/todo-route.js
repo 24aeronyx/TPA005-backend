3ec57d3e0c5f4e4137a364a291f1ae65ffd15bef
24aeronyx <11211017@student.itk.ac.id>
2023-11-14 00:51:25 +0800

    Add routes todo

--- routes/todo-route.js
+++ routes/todo-route.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const route = express.Router()
 
-const { getAllTodo, getTodoById, addTodo } = require('../controllers/todo-controller')
+const { getAllTodo, getTodoById, addTodo, editTodoById, deleteAllTodo, deleteTodoById } = require('../controllers/todo-controller')
 
 route.get("/", getAllTodo)
 route.get("/:id", getTodoById)
 route.post("/", addTodo)
-route.get("/", )
+route.put("/:id", editTodoById )
+route.delete("/", deleteAllTodo)
+route.delete("/:id", deleteTodoById)
 
 module.exports = route
